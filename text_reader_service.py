ec74bb729096a445ef2771f0d52a6f7a39a06d2d
Richard Szilagyi <szpatrichard@proton.me>
2024-08-16 17:21:35 +0100

    Add docstrings to TextReaderService methods and class

--- text_reader_service.py
+++ text_reader_service.py
@@ -1,8 +1,9 @@
-import grpc
 from concurrent import futures
+import logging
+
+import grpc
 import text_reader_pb2
 import text_reader_pb2_grpc
-import logging
 
 from utils import constants
 
@@ -10,17 +11,22 @@ logging.basicConfig(level=logging.INFO)
 
 
 class TextReaderService(text_reader_pb2_grpc.TextReaderServicer):
+    """Provides methods that implement functionality of TextReader service."""
+
     def __init__(self, file_path):
+        """Initializes the TextReaderService with the path to the file to read."""
         self.file_path = file_path
         self.words = self._load_words()
         logging.info("Loaded %d words from %s", len(self.words), file_path)
 
     def _load_words(self):
+        """Loads the words from the file and returns them as a list."""
         with open(self.file_path, "r", encoding="utf-8") as file:
             text = file.read()
         return text.split()
 
     def GetNextWord(self, request, context):
+        """Returns the next word from the file."""
         if not self.words:
             logging.info("Reached end of file")
             return text_reader_pb2.WordResponse(word="", eof=True)
@@ -30,6 +36,7 @@ class TextReaderService(text_reader_pb2_grpc.TextReaderServicer):
 
 
 def serve():
+    """Starts the gRPC server to serve the TextReader service."""
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     text_reader_service = TextReaderService(constants.BOOK_PATH)
     text_reader_pb2_grpc.add_TextReaderServicer_to_server(text_reader_service, server)
